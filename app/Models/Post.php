90962d80a28b913dbdc9d656d5ff508aba0ec86f
AnthonyAndino <anthonyandino959@gmail.com>
2026-05-17 21:57:27 -0600

    Add Post model and posts migration

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'content',
+        'user_id',
+        'status',
+        'image',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published-at' => 'datetime'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
